d9c283788617c9ad57a5a871e5d81d50918ee0c1
funnyboy-roks <funnyboyroks@gmail.com>
2021-09-13 08:34:11 -0500

    Update PC Setup

--- bash/pc-setup.sh
+++ bash/pc-setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # A script to setup the basics of my pc
+# Run with curl -sL https://raw.githubusercontent.com/funnyboy-roks/personal-config/main/bash/pc-setup.sh | bash
 
 sudo apt update
 sudo apt upgrade
@@ -17,8 +18,22 @@ sudo ln -s /opt/firefox/firefox /usr/bin/firefox
 # Install Discord
 sudo snap install discord
 
+# Install AutoKey
+sudo apt install autokey-qt
+cd ~/.config/autokey/data
+curl -o .PipeChar.json https://gist.githubusercontent.com/funnyboy-roks/fa2f5e4c03ddf5bdb71c60c441e552b0/raw/30d177cc3308f8d094bf3902a13d3850c9dc20b3/.PipeChar.json
+echo '|' >> PipeChar.txt
+curl -o .Backslash.json https://gist.githubusercontent.com/funnyboy-roks/2cfa90233aaa0d0c40105dd405f05124/raw/ba064f4c670235fadc97db78ba5facfd2ec88134/.Backslash.json
+echo '\' >> Backslash.txt
+cd ~
+
 # Install NodeJS
-sudo apt install nodejs -y
+sudo apt install nodejs npm -y
+# Install Nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+
+# Basic npm packages
+sudo npm i -g npm license gitignore lite-server
 
 # Install nemo
 sudo apt install nemo -y
