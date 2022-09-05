8518554244531f5046e9354ab19f86ba88f5f78a
funnyboy-roks <funnyboyroks@gmail.com>
2022-09-05 11:58:17 -0500

    Config Updates

---
+++ i3/i3blocks/install_battery-plus.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+sudo wget -O /usr/share/i3blocks/battery-plus https://raw.githubusercontent.com/hastinbe/i3blocks-battery-plus/master/battery-plus
+sudo chmod +x /usr/share/i3blocks/battery-plus
