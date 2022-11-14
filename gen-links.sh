923024cd3b0480270f2c762e1f306a7103aef502
funnyboy-roks <funnyboyroks@gmail.com>
2022-11-14 11:36:12 -0600

    Add vimrc and update dustrc

--- gen-links.sh
+++ gen-links.sh
@@ -17,6 +17,7 @@ done
 ln -s "$HERE/zsh/config.zsh" "$ZSH/custom/config.zsh"
 ln -s "$HERE/zsh/.zshrc" "$HOME/.zshrc"
 ln -s "$HERE/zsh/.p10k.zsh" "$HOME/.p10k.zsh"
+ln -s "$HERE/.vimrc" "$HOME/.vimrc"
 
 if ! [ -e "$HOME/.config/ulauncher" ]; then
     ln -s "$HERE/ulauncher" "$HOME/.config/ulauncher"
