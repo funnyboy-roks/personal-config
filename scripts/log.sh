359c2e1ce02b8c16719acfc4804abb1b8a6f07a1
funnyboy-roks <funnyboyroks@gmail.com>
2023-12-11 15:41:20 -0600

    log.sh: remove explicit timezone

--- scripts/log.sh
+++ scripts/log.sh
@@ -2,5 +2,5 @@
 # Write a message to my custom "system log" (~/log)
 # Usage: log.sh <message>
 
-time="$(date -uI'seconds')"
+time="$(date -uI'seconds' | cut -d'+' -f1)"
 echo "[$time] $*" >> "$HOME/log"
